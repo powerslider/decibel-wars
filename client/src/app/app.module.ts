5d8b185c505b23179d53a5e93e1180f3b139aae9
Aleksandar Savev <savevjr@gmail.com>
2017-02-15 22:23:05 +0200

    Add base routing

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 /**
  * Material imports
@@ -15,19 +16,30 @@ import { FlexLayoutModule} from '@angular/flex-layout';
 import { AppComponent } from './app.component';
 import { DecibelToolbarComponent } from './decibel-toolbar/decibel-toolbar.component';
 import { SongCardComponent } from './song-card/song-card.component';
+import { RequestSongComponent } from './request-song/request-song.component';
+import { SearchSongComponent } from './search-song/search-song.component';
+
+const appRoutes: Routes = [
+  { path: 'search-song', component: SearchSongComponent },
+  { path: 'request-song', component: RequestSongComponent },
+  { path: '',   redirectTo: '/search-song', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     DecibelToolbarComponent,
-    SongCardComponent
+    SongCardComponent,
+    RequestSongComponent,
+    SearchSongComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     MaterialModule.forRoot(),
-    FlexLayoutModule.forRoot()
+    FlexLayoutModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
