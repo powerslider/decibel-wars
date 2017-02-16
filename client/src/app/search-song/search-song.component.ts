3cdb1c931b8d618b3c7fae2bfd65067378f21b82
Aleksandar Savev <savevjr@gmail.com>
2017-02-17 00:58:19 +0200

    Get songs over http

--- client/src/app/search-song/search-song.component.ts
+++ client/src/app/search-song/search-song.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Song } from '../model/song';
+import { SongsService } from '../songs.service';
 
 @Component({
   selector: 'app-search-song',
@@ -9,12 +10,14 @@ import { Song } from '../model/song';
 export class SearchSongComponent implements OnInit {
 
   private songs: Array<Song> = [];
-  constructor() { }
+  @Input() private searchText: string;
+  constructor(private songsService: SongsService) { }
 
   ngOnInit() {
   }
 
   private onSearchClicked() {
+    this.songsService.getSongs(this.searchText);
     this.songs = [
       { title: "When I'm gone", artist: "Eminem", year: 2005 },
       { title: "Not afraid", artist: "Eminem", year: 2010 }
