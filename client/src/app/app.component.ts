7a6b56ac8598ca2cc4c3810518946f61038caca8
Aleksandar Savev <savevjr@gmail.com>
2017-02-17 09:38:21 +0200

    Add LoginService provider

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,11 +1,12 @@
-import {Component} from '@angular/core';
-import {SongsService} from './songs.service'
+import { Component } from '@angular/core';
+import { SongsService } from './songs.service';
+import { LoginService } from './login.service';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
-    providers: [SongsService]
+    providers: [SongsService, LoginService]
 })
 export class AppComponent {
     title = 'app works!';
